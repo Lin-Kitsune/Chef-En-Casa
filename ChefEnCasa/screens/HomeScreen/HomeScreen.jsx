0300c5d2cadc0fa356d04e8c9047f87b75bd06ed
Lin-Kitsune <kitsunelin567@gmail.com>
2024-10-15 20:45:38 -0300

    Se arreglaron cosas de salud y home

--- ChefEnCasa/screens/HomeScreen/HomeScreen.jsx
+++ ChefEnCasa/screens/HomeScreen/HomeScreen.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; // Añade useState aquí
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './HomeScreenStyles'; // Importa los estilos
 import { useNavigation } from '@react-navigation/native';  
+import { useFocusEffect } from '@react-navigation/native';
+import { getUserProfile } from '../../services/auth'; 
 
 const HomeScreen = () => {
   const navigation = useNavigation();  // Hook para la navegación
+  const [userName, setUserName] = useState('Usuario');
+  const [diet, setDiet] = useState('Sin dieta');   
   
+// Efecto para cargar los datos del usuario al montar el componente
+useFocusEffect(
+  React.useCallback(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const userProfile = await getUserProfile();  // Obtener el perfil del usuario
+        
+        // Actualizar los datos de nombre y dieta
+        setUserName(userProfile.nombre || 'Usuario'); // Si no hay nombre, poner 'Usuario' por defecto
+        setDiet(userProfile.healthData?.dietRecommendation || 'Sin dieta'); // Usar la recomendación de dieta o 'Sin dieta'
+        
+      } catch (error) {
+        console.error('Error al obtener el perfil del usuario', error);
+      }
+    };
+
+    fetchUserProfile(); // Llamar a la función cuando se enfoque la pantalla
+
+  }, []) // El array vacío asegura que esto ocurra cada vez que la pantalla esté enfocada
+); // Ejecutar solo una vez al montar el componente
+
   return (
     <View style={styles.container}>
       {/* Encabezado */}
@@ -18,9 +43,9 @@ const HomeScreen = () => {
         <View style={styles.userInfoContainer}>
           <Image source={require('../../assets/images/user-avatar.png')} style={styles.avatar} />
           <View style={styles.userInfo}>
-            {/* Texto envuelto en un componente <Text> */}
-            <Text style={styles.userName}>User</Text>
-            <Text style={styles.dietText}>Dieta equilibrada</Text>
+             {/* Mostrar el nombre del usuario y la dieta */}
+             <Text style={styles.userName}>{userName}</Text>
+            <Text style={styles.dietText}>{diet}</Text>
           </View>
         </View>
       </View>
