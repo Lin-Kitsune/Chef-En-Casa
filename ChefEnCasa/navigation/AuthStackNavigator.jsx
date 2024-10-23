c83e8234f9df6f61d5ca61bb00d8af6dd523d951
FranViera <129343029+FranViera@users.noreply.github.com>
2024-10-23 00:12:00 -0300

    Update AuthStackNavigator.jsx
    Se agrega Logout

--- ChefEnCasa/navigation/AuthStackNavigator.jsx
+++ ChefEnCasa/navigation/AuthStackNavigator.jsx
@@ -33,7 +33,24 @@ const AuthStackNavigator = () => {
           }}
       />
     </Stack.Navigator>
+    
   );
+};
+
+const handleLogout = async () => {
+  try {
+    await AsyncStorage.removeItem('userToken'); // Eliminar el token de AsyncStorage
+    
+    // Cambiar al stack de autenticación
+    navigation.dispatch({
+      type: 'RESET',
+      index: 0,
+      routes: [{ name: 'Login' }],  // Asegúrate de que 'Login' está registrado en el AuthStackNavigator
+    });
+    
+  } catch (error) {
+    console.error('Error al cerrar sesión:', error);
+  }
 };
 
 export default AuthStackNavigator;
