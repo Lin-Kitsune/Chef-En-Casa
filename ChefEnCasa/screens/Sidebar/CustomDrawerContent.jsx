672179455d15c156046bec2de3a8a6ae5de9da1c
FranViera <129343029+FranViera@users.noreply.github.com>
2024-10-23 00:09:50 -0300

    Update CustomDrawerContent.jsx
    Se agrega Logout

--- ChefEnCasa/screens/Sidebar/CustomDrawerContent.jsx
+++ ChefEnCasa/screens/Sidebar/CustomDrawerContent.jsx
@@ -1,61 +1,67 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { DrawerContentScrollView } from '@react-navigation/drawer'; 
-import styles from './CustomStyles'; 
+import { DrawerContentScrollView } from '@react-navigation/drawer';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import styles from './CustomStyles';
 
 const CustomDrawerContent = (props) => {
-  const { navigation, state } = props;
+  const { navigation, state, handleLogout } = props;  // Recibir `handleLogout` aquí
 
-  // Función para verificar si un ítem está activo
   const isActive = (routeName) => {
     const currentRoute = state?.routeNames[state?.index];
     return currentRoute === routeName;
   };
 
+  const handleSignOut = async () => {
+    try {
+      await AsyncStorage.removeItem('userToken');
+      handleLogout();  // Llamar a la función que cambia el estado
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+    }
+  };
+
   return (
-    <DrawerContentScrollView {...props}> 
+    <DrawerContentScrollView {...props}>
       <View style={styles.container}>
-        {/* Contenido principal del Drawer */}
         <View style={styles.contentContainer}>
-          {/* Logo o Encabezado */}
           <View style={styles.header}>
             <View style={styles.logoRow}>
               <Image
                 source={require('../../assets/images/Chef-En-Casa.png')}
                 style={styles.appIcon}
               />
-              <Text style={styles.logoText}>Chef En Casa</Text> 
+              <Text style={styles.logoText}>Chef En Casa</Text>
             </View>
             <View style={styles.divider} />
           </View>
 
-          {/* Enlaces del Drawer */}
-          <TouchableOpacity 
-            style={[styles.drawerItem, isActive('NewsScreen') ? styles.activeItem : null]} 
+          <TouchableOpacity
+            style={[styles.drawerItem, isActive('NewsScreen') ? styles.activeItem : null]}
             onPress={() => navigation.navigate('NewsStack')}
           >
             <Icon name="newspaper-o" size={24} color={isActive('NewsScreen') ? "#FFF" : "#619537"} />
             <Text style={[styles.drawerText, isActive('NewsScreen') ? styles.activeText : null]}>
               Noticias
-            </Text> 
+            </Text>
           </TouchableOpacity>
 
-          <TouchableOpacity 
-            style={[styles.drawerItem, isActive('PointsStack') ? styles.activeItem : null]} 
+          <TouchableOpacity
+            style={[styles.drawerItem, isActive('PointsStack') ? styles.activeItem : null]}
             onPress={() => navigation.navigate('PointsStack')}
           >
             <Icon name="star" size={24} color={isActive('Points') ? "#FFF" : "#EFB810"} />
             <Text style={[styles.drawerText, isActive('Points') ? styles.activeText : null]}>
               Puntos
-            </Text> 
+            </Text>
           </TouchableOpacity>
         </View>
-        {/* Botón de cerrar sesión en la parte inferior */}
+
         <View style={styles.logoutContainer}>
-          <TouchableOpacity style={styles.logout} onPress={() => {}}>
+          <TouchableOpacity style={styles.logout} onPress={handleSignOut}>
             <Icon name="sign-out" size={24} color="#888" />
-            <Text style={styles.drawerText}>Cerrar sesión</Text> 
+            <Text style={styles.drawerText}>Cerrar sesión</Text>
           </TouchableOpacity>
         </View>
       </View>
