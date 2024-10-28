32f516334b3dc6651b41467f144a7cb0d4a64ef3
FranViera <129343029+FranViera@users.noreply.github.com>
2024-10-28 12:09:32 -0300

    Update ingredients.jsx
    Se cargan las funciones desde la API del Back para el trato de ingredientes

--- ChefEnCasa/services/ingredients.jsx
+++ ChefEnCasa/services/ingredients.jsx
@@ -1,12 +1,39 @@
 import axios from 'axios';
+import { getToken } from './auth';  // Importamos la función para obtener el token
 
 const API_URL = 'http://10.0.2.2:4000'; // Cambia esto a localhost o 10.0.2.2
 
 export const fetchIngredients = async () => {
   try {
-    const response = await axios.get(`${API_URL}/ingredientes`);
-    return response.data.results; // Retorna los ingredientes que has obtenido
+    const token = await getToken();
+    const response = await axios.get(`${API_URL}/ingredientes`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    console.log('Respuesta de la API de ingredientes:', response.data);
+
+    if (!response.data || !response.data.results) {
+      throw new Error('Formato de respuesta no válido');
+    }
+
+    return response.data.results;
   } catch (error) {
-    throw new Error('Error al obtener los ingredientes'); // Lanza un error si ocurre algo
+    console.error('Error al obtener los ingredientes:', error);
+    throw new Error('Error al obtener los ingredientes');
+  }
+};
+
+export const eliminarIngrediente = async (nombreIngrediente) => {
+  try {
+    const token = await getToken();  // Obtener el token del usuario
+    const response = await axios.delete(`${API_URL}/almacen/eliminar`, {
+      headers: { Authorization: `Bearer ${token}` },
+      data: { nombreIngrediente }
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error('Error al eliminar el ingrediente');
   }
 };
