687c197d12d722589e875bf7fdd64524b078eaf7
FranViera <129343029+FranViera@users.noreply.github.com>
2024-10-29 01:51:53 -0300

    Update BuscadorNavigator.jsx

--- ChefEnCasa/navigation/BuscadorNavigator.jsx
+++ ChefEnCasa/navigation/BuscadorNavigator.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import BuscadorScreen from '../screens/BuscadorScreen/BuscadorScreen';
+import RecipeDetailScreen from '../screens/RecipeDetailScreen/RecipeDetailScreen'; // Importa la pantalla de detalles
 
 const Stack = createStackNavigator();
 
@@ -10,14 +12,12 @@ const BuscadorNavigator = () => {
       <Stack.Screen
         name="BuscadorScreen"
         component={BuscadorScreen}
-        options={{
-          title: 'BUSCAR',
-          headerStyle: {
-            backgroundColor: '#619537',
-          },
-          headerTintColor: '#fff',
-          headerTitleAlign: 'center',
-        }}
+        options={{ title: 'Buscar Recetas' }}
+      />
+      <Stack.Screen
+        name="RecipeDetail"
+        component={RecipeDetailScreen} // Registra la pantalla de detalles aquí
+        options={{ title: 'Detalles de la Receta' }}
       />
     </Stack.Navigator>
   );
