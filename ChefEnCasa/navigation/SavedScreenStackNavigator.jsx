7be45e35c67f76b9d8cfd9e75b9e2503505d1854
FranViera <129343029+FranViera@users.noreply.github.com>
2024-10-30 03:22:17 -0300

    Update SavedScreenStackNavigator.jsx

--- ChefEnCasa/navigation/SavedScreenStackNavigator.jsx
+++ ChefEnCasa/navigation/SavedScreenStackNavigator.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import SavedScreen from '../screens/SavedScreen/SavedScreen';
+import RecipeDetailScreen from '../screens/RecipeDetailScreen/RecipeDetailScreen';
 
 const Stack = createStackNavigator();
 
 const SavedScreenStackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="SavedScreen"
-        component={SavedScreen}
-        options={{
-          title: 'GUARDADOS',
-          headerStyle: {
-            backgroundColor: '#619537',
-          },
-          headerTintColor: '#fff',
-          headerTitleAlign: 'center',
-        }}
+      <Stack.Screen 
+        name="SavedScreen" 
+        component={SavedScreen} 
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="RecipeDetail" 
+        component={RecipeDetailScreen} 
+        options={{ title: 'Detalle de la Receta' }} 
       />
     </Stack.Navigator>
   );
