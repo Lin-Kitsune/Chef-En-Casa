717188c80025f04c357c09c6d7be31e6a01881d5
Lin-Kitsune <kitsunelin567@gmail.com>
2024-12-03 00:16:27 -0300

    Se arreglo el aprendizaje

--- adminweb/src/components/SabiasQue/SabiasQue.jsx
+++ adminweb/src/components/SabiasQue/SabiasQue.jsx
@@ -2,32 +2,33 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const IMAGE_BASE_URL = 'http://localhost:4000';
+const IMAGE_BASE_URL = 'http://localhost:4000'; // Asegúrate que esta ruta esté bien configurada en tu servidor
 
 const SabiasQue = ({ data, onEdit, onDelete }) => {
+
   return (
     <div className="bg-white rounded-lg shadow-md p-4 flex flex-col h-full">
       <div className="flex-grow">
         <img
           src={data.imagen ? `${IMAGE_BASE_URL}/${data.imagen}` : 'ruta/a/imagen/por/defecto.jpg'}
           alt={data.titulo}
-          className="w-full h-32 object-cover mb-4 rounded-lg"
+          className="w-full h-48 object-cover mb-4 rounded-lg"
           crossOrigin="anonymous"
         />
         <p className="text-lg font-semibold">{data.titulo}</p>
         <p className="text-sm text-gray-600">{data.descripcion}</p>
-        <p className="text-sm text-gray-500 mt-2">{data.beneficio}</p> {/* Mostramos el beneficio */}
+        <p className="text-sm text-gray-500 mt-2">{data.beneficio}</p>
       </div>
       <div className="mt-4 flex space-x-4 justify-between">
         <button
-            className="bg-verde-chef text-white py-2 px-4 rounded-full font-bold hover:bg-blue-600"
-            onClick={() => onEdit(data)}  // Botón de editar
+          className="bg-verde-chef text-white py-2 px-4 rounded-full font-bold hover:bg-blue-600"
+          onClick={() => onEdit(data)}  // Botón de editar
         >
           <FontAwesomeIcon icon={faEdit} /> Editar
         </button>
         <button
-            className="bg-red-500 text-white px-4 py-2 rounded-full"
-            onClick={() => onDelete(data._id)} // Asegúrate de pasar _id
+          className="bg-red-500 text-white px-4 py-2 rounded-full"
+          onClick={() => onDelete(data._id)} // Asegúrate de pasar _id
         >
           <FontAwesomeIcon icon={faTrash} /> Eliminar
         </button>
